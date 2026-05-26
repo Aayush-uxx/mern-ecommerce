287807b7c41c7bb51c70a0b4c79735fa565ffb14
Aayush <xaayush977@gmail.com>
2026-05-26 10:13:05 +0545

    Fixed the authMiddleware for admin

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,20 +1,34 @@
 import jwt from "jsonwebtoken";
-import user from "../models/user.js";
+import users from "../models/user.js";
 
 const authAdmin = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
+
     if (!token) {
-      res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
     const decode = jwt.verify(token, process.env.JWT_SECRET);
+
     if (!decode) {
-      res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
-    req.user = await user.findById(decode.id).select("password");
+    req.user = await users.findById(decode.id).select("-password");
+    console.log("User found:", req.user);
     next();
   } catch (error) {
     res.status(400).json({ message: "Unauthorized Invalid token !" });
   }
 };
-export default authAdmin;
+const adminCheck = async (req, res, next) => {
+  try {
+    if (!req.user || req.user.isAdmin !== true) {
+      return res.status(403).json({ message: "Admin access only" });
+    }
+    next();
+  } catch (error) {
+    res.status(403).json({ message: error.message });
+  }
+};
+
+export { authAdmin, adminCheck };
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -6,12 +6,13 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controller/productController.js";
+import { authAdmin, adminCheck } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.get("/", getProducts);
 router.get("/:id", getProductById);
-router.post("/", createProduct);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.post("/", authAdmin, adminCheck, createProduct);
+router.put("/:id", authAdmin, adminCheck, updateProduct);
+router.delete("/:id", authAdmin, adminCheck, deleteProduct);
 
 export default router;
